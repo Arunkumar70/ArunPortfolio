3c93aa799ed00becfe6f1e98b66648245ea967a8
Arun-kumar-commits <123375071+Arun-kumar-commits@users.noreply.github.com>
2024-06-03 16:12:06 +0530

    resumeadded

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -19,8 +19,10 @@ function Home({handleScroll}) {
                         <h3 style={{whiteSpace: "nowrap" }}>Arun kumar H S </h3><br/>
                         <h4 style={{whiteSpace: "nowrap", color:'gray' }}>AR/VR/MR and FrontEnd Developer </h4><br/>
                         <div>
-                            <button type="button" className="btn btn-outline-dark rounded-pill" style={{margin:'10px'}}>Download CV</button>
-                            <button onClick={()=>handleScroll('Contact')} type="button" className="btn btn-outline-dark rounded-pill" style={{margin:'10px'}}>Contact Me</button>
+                            <a href='https://drive.google.com/file/d/1tQ1pPAjnKuv41G8VJZHw4Gtz6fLcbVEC/view?usp=sharing' >
+                            <button type="button" className="btn btn-outline-dark rounded-pill" style={{margin:'10px'}}>
+                               Download CV</button> </a>
+                            <button onClick={()=>handleScroll('Contact')} type="button" className="btn btn-outline-dark rounded-pill" style={{margin:'10px'}}> Contact Me</button>
                         </div>
                         <div className='d-flex justify-content-center'>
                             <div className="p-3"><a href="" title="TVmaze Facebook page" ><FaLinkedin style={{width:'190%', height:"190%", color: 'black'}}/></a></div>
