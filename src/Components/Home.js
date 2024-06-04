6ae5747f6c79338f8a8c0e0e475dab216d95bce4
Arun-kumar-commits <123375071+Arun-kumar-commits@users.noreply.github.com>
2024-06-04 14:02:26 +0530

    resumenewadded

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -19,7 +19,7 @@ function Home({handleScroll}) {
                         <h3 style={{whiteSpace: "nowrap" }}>Arun kumar H S </h3><br/>
                         <h4 style={{whiteSpace: "nowrap", color:'gray' }}>AR/VR/MR and FrontEnd Developer </h4><br/>
                         <div>
-                            <a href='https://drive.google.com/file/d/1tQ1pPAjnKuv41G8VJZHw4Gtz6fLcbVEC/view?usp=sharing' >
+                            <a href='https://drive.google.com/file/d/1XeSZUHaBajqE6oBijN4ez5Z9WuCCzgx8/view?usp=sharing' >
                             <button type="button" className="btn btn-outline-dark rounded-pill" style={{margin:'10px'}}>
                                Download CV</button> </a>
                             <button onClick={()=>handleScroll('Contact')} type="button" className="btn btn-outline-dark rounded-pill" style={{margin:'10px'}}> Contact Me</button>
